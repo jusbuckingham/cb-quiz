ccd50997f2235711170432899bfeab18d6f9773c
jusbuckingham <jus.buckingham@gmail.com>
2024-01-26 08:13:12 -0800

    adding imgs and trying to compile

--- components/SkinCareOptions.js
+++ components/SkinCareOptions.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styles from '../styles/SkinCareOptions.module.css'; // Import the CSS module
+import styles from './SkinCareOptions.module.css';
 
 const SkinCareOptions = () => {
   const [selectedGoal, setSelectedGoal] = useState(null);
@@ -11,17 +11,17 @@ const SkinCareOptions = () => {
   };
 
   return (
-    <div className={`${styles.container} bg-primary-color text-text-color`}>
-      <div className={`${styles.leftPanel} text-center`}>
+    <div className={styles.container}>
+      <div className={styles.leftPanel}>
         <h1>Achieve Perfectly Smooth, Youthful Skin</h1>
         <p>Get a personalized skin care treatment</p>
       </div>
       <h2>Choose Your Goal</h2>
-      <div className={`${styles.rightPanel} w-full`}>
-        <div className={`${styles.options} flex flex-col w-full`}>
-          <button className={`${styles.option} my-2 py-2 border-2 border-option-border-color text-text-color text-center cursor-pointer transition duration-300 hover:bg-primary-color hover:text-white`} onClick={() => handleGoalSelection('Remove Crepey Skin')}>Remove Crepey Skin</button>
-          <button className={`${styles.option} my-2 py-2 border-2 border-option-border-color text-text-color text-center cursor-pointer transition duration-300 hover:bg-primary-color hover:text-white`} onClick={() => handleGoalSelection('Relieve Dry, Flaky Skin')}>Relieve Dry, Flaky Skin</button>
-          <button className={`${styles.option} my-2 py-2 border-2 border-option-border-color text-text-color text-center cursor-pointer transition duration-300 hover:bg-primary-color hover:text-white`} onClick={() => handleGoalSelection('Get Firmer, Tighter, Younger Looking Skin')}>Get Firmer, Tighter, Younger Looking Skin</button>
+      <div className={styles.rightPanel}>
+        <div className={styles.options}>
+          <button className={styles.option} onClick={() => handleGoalSelection('Remove Crepey Skin')}>Remove Crepey Skin</button>
+          <button className={styles.option} onClick={() => handleGoalSelection('Relieve Dry, Flaky Skin')}>Relieve Dry, Flaky Skin</button>
+          <button className={styles.option} onClick={() => handleGoalSelection('Get Firmer, Tighter, Younger Looking Skin')}>Get Firmer, Tighter, Younger Looking Skin</button>
         </div>
       </div>
     </div>
