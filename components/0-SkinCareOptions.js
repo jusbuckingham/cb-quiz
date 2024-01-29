8f34c9013f9f451f02060ee0de28f3aa553d22f6
jusbuckingham <jus.buckingham@gmail.com>
2024-01-29 10:40:29 -0800

    refining 0

--- components/0-SkinCareOptions.js
+++ components/0-SkinCareOptions.js
@@ -1,5 +1,6 @@
+// SkinCareOptions.js
 import React, { useState } from 'react';
-import styles from '../styles/0-SkinCareOptions.module.css'
+import styles from '../styles/0-SkinCareOptions.module.css'; // Adjust the import path as necessary
 
 const SkinCareOptions = ({ onContinue }) => {
   const [selectedGoal, setSelectedGoal] = useState(null);
@@ -11,9 +12,21 @@ const SkinCareOptions = ({ onContinue }) => {
 
   // Define your goals and corresponding images
   const goals = [
-    { id: 'removeCrepeySkin', label: 'Remove Crepey Skin' , imageSrc: '/path-to-remove-crepey-skin-image.jpg' },
-    { id: 'relieveDrySkin', label: 'Relieve Dry, Flaky Skin', imageSrc: '/path-to-relieve-dry-skin-image.jpg' },
-    { id: 'getFirmerSkin', label: 'Get Firmer, Tighter, Younger Looking Skin', imageSrc: '/path-to-get-firmer-skin-image.jpg' },
+    {
+      id: 'removeCrepeySkin',
+      label: 'Remove Crepey Skin',
+      imageSrc: '/cb-quiz-frontend-imgs/0-remove-crepy-skin.png'
+    },
+    {
+      id: 'relieveDrySkin',
+      label: 'Relieve Dry, Flaky Skin',
+      imageSrc: '/cb-quiz-frontend-imgs/0-relieve-dry-flaky-skin.png'
+    },
+    {
+      id: 'getFirmerSkin',
+      label: 'Get Firmer, Tighter, Younger Looking Skin',
+      imageSrc: '/cb-quiz-frontend-imgs/0-get-firmer-tighter-younger-looking-skin.png'
+    },
   ];
 
   return (
@@ -26,12 +39,17 @@ const SkinCareOptions = ({ onContinue }) => {
         <h2 className={styles.title}>Choose Your Goal</h2>
         <div className={styles.goalOptions}>
           {goals.map((goal) => (
-            <div 
+            <div
               key={goal.id}
               className={`${styles.option} ${selectedGoal === goal.id ? styles.active : ''}`}
               onClick={() => handleGoalSelection(goal.id)}
             >
               <span className={styles.optionText}>{goal.label}</span>
+              <img
+                src={goal.imageSrc}
+                alt={goal.label}
+                className={styles.optionImage}
+              />
             </div>
           ))}
         </div>
