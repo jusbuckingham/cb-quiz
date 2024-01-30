b7ddf299e3fe05d9c6e03663ffbc853094f63a3a
jusbuckingham <jus.buckingham@gmail.com>
2024-01-29 19:39:00 -0800

    still needs styling work but the functionality is finnished

--- components/7-SkincareExperienceQuestion.js
+++ components/7-SkincareExperienceQuestion.js
@@ -1,11 +1,9 @@
-// 7-SkincareExperienceQuestion.js
-
 import React, { useState } from 'react';
 import styles from '../styles/7-SkincareExperienceQuestion.module.css';
 
 const SkincareExperienceQuestion = ({ onSubmit }) => {
   const [selectedOptions, setSelectedOptions] = useState({});
-  const [submissionMessage, setSubmissionMessage] = useState("");
+  const [showSubmissionMessage, setShowSubmissionMessage] = useState(false);
 
   const options = [
     'Overwhelmed, too many products to keep track of',
@@ -17,53 +15,55 @@ const SkincareExperienceQuestion = ({ onSubmit }) => {
   ];
 
   const handleOptionChange = (option) => {
-    const newSelectedOptions = { ...selectedOptions, [option]: !selectedOptions[option] };
+    setSelectedOptions((prevSelectedOptions) => ({
+      ...prevSelectedOptions,
+      [option]: !prevSelectedOptions[option]
+    }));
 
+    // Trigger the submission message only if "None of the Above" is selected
     if (option === 'None of the Above') {
-      // If 'None of the Above' is selected, clear all other selections.
-      if (newSelectedOptions[option]) {
-        for (let key in newSelectedOptions) {
-          if (key !== 'None of the Above') {
-            newSelectedOptions[key] = false;
-          }
-        }
-        setSubmissionMessage("Thanks for sharing! We've discovered that a lot of people have faced the same setbacks. City Beauty has a clear one-step treatment that is easy to follow with long-term results. In less than two minutes, you will feel better than ever.");
-      } else {
-        setSubmissionMessage("");
-      }
+      setShowSubmissionMessage(!selectedOptions[option]);
     } else {
-      // If any other option is selected, 'None of the Above' should not be selected.
-      newSelectedOptions['None of the Above'] = false;
-      setSubmissionMessage("");
+      // If other options are selected, ensure "None of the Above" is not selected
+      setSelectedOptions((prevSelectedOptions) => ({
+        ...prevSelectedOptions,
+        'None of the Above': false
+      }));
+      setShowSubmissionMessage(false);
     }
-
-    setSelectedOptions(newSelectedOptions);
   };
 
   const handleSubmit = () => {
+    // Example submit function, replace with actual submit logic
     onSubmit(selectedOptions);
+    setShowSubmissionMessage(true);
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.progress}>6/6</div>
       <h2>Have you experienced any of these issues in your previous skincare attempts?</h2>
-      {options.map((option, index) => (
-        <label key={index} className={`${styles.option} ${selectedOptions[option] ? styles.optionSelected : ''}`}>
-          <input
-            type="checkbox"
-            checked={!!selectedOptions[option]}
-            onChange={() => handleOptionChange(option)}
-            className={styles.checkbox}
-          />
-          {option}
-        </label>
-      ))}
-      <div className={styles.submissionMessage} style={{ display: submissionMessage ? 'block' : 'none' }}>
-        {submissionMessage}
-      </div>
-      <button className={styles.continueButton} onClick={handleSubmit}>Continue</button>
-      <button className={styles.skipButton} onClick={onSubmit}>Skip</button>
+      <form onSubmit={handleSubmit}>
+        {options.map((option, index) => (
+          <div key={index} className={`${styles.option} ${selectedOptions[option] ? styles.optionSelected : ''}`}>
+            <label>
+              <input
+                type="checkbox"
+                name={option}
+                checked={!!selectedOptions[option]}
+                onChange={() => handleOptionChange(option)}
+                className={styles.checkbox}
+              />
+              {option}
+            </label>
+          </div>
+        ))}
+        <div className={styles.submissionMessage} style={{ display: showSubmissionMessage ? 'block' : 'none' }}>
+          Thanks for sharing! We've discovered that a lot of people have faced the same setbacks. City Beauty has a clear one-step treatment that is easy to follow with long-term results. In less than two minutes, you will feel better than ever.
+        </div>
+        <button type="submit" className={styles.continueButton}>Continue</button>
+        <button type="button" className={styles.skipButton} onClick={onSubmit}>Skip</button>
+      </form>
     </div>
   );
 };
